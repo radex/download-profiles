223c242d4f2a0c51e43c8fd775925caab849ef77
radex <this.is@radex.io>
2014-12-18 19:16:51 +0100

    download command options

--- lib/download-profiles/provisioning_portal/commands/profiles.rb
+++ lib/download-profiles/provisioning_portal/commands/profiles.rb
@@ -1,11 +1,14 @@
 command :download do |c|
   c.syntax = 'download-profiles'
-  c.summary = 'Downloads all the active Provisioning Profiles'
+  c.summary = 'Download provisioning profiles'
+  c.description = "By default, downloads and installs all (development, distribution, iOS, Mac) provisioning profiles. Pass appropriate options to constrain."
 
-  c.option '--type [TYPE]', [:development, :distribution], "Type of profile (development or distribution; defaults to development)"
+  c.option '--platform [PLATFORM]', [:ios, :mac, :all], "Platform of profile (ios, mac or all)"
+  c.option '--type [TYPE]', [:development, :distribution, :all], "Type of profile (development, distribution or all)"
 
   c.action do |args, options|
-    type = (options.type.downcase.to_sym if options.type) || :development
+    type = (options.type.downcase.to_sym if options.type) || :all
+    platform = (options.platform.downcase.to_sym if options.platform) || :all
     profiles = try{agent.list_profiles(type)}.select{|profile| profile.status == 'Active'}
 
     say_warning "No active #{type} profiles found." and abort if profiles.empty?
