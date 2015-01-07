67885f149e670473690dac2b369479ef406032d7
radex <this.is@radex.io>
2015-01-07 13:37:28 +0100

    Status notice

--- lib/download-profiles/provisioning_portal/commands/profiles.rb
+++ lib/download-profiles/provisioning_portal/commands/profiles.rb
@@ -7,24 +7,35 @@ command :download do |c|
   c.option '--type [TYPE]', [:development, :distribution, :all], "Type of profile (development, distribution or all)"
 
   c.action do |args, options|
+    # parse options
     platform = options.platform ? options.platform.downcase.to_sym : :all
     type = options.type ? options.type.downcase.to_sym : :all
     
     platforms = (platform == :all) ? [:ios, :mac] : [platform]
     types = (type == :all) ? [:development, :distribution] : [type]
     
+    # get list of profiles
     profiles = 
       platforms.product(types).flat_map { |platform, type| try{agent.list_profiles(platform, type)} }
       .select { |profile| profile.status == 'Active'}
     
     say_warning "No active #{type} profiles found." and abort if profiles.empty?
     
+    # download
+    failure = false
     profiles.each do |profile|
       if filename = agent.download_profile(profile)
-        say_ok "Successfully downloaded: '#{filename}'"
+        say "Downloaded #{filename}"
       else
         say_error "Could not download profile: '#{profile.name}'"
+        failure = true
       end
+    end
+    
+    if failure
+      say_warning "Not all profiles downloaded successfully"
+    else
+      say_ok "All profiles downloaded successfully"
     end
   end
 end
